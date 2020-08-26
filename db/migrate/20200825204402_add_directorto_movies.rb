8e4208e8112360043aec2828ea2331b08c4c7917
estein1988 <estein1988@gmail.com>
2020-08-26 08:35:22 -0600

    Complete rails boiler plate setup one-to-one relationship

---
+++ db/migrate/20200825204402_add_directorto_movies.rb
@@ -0,0 +1,5 @@
+class AddDirectortoMovies < ActiveRecord::Migration[6.0]
+  def change
+    add_column :movies, "director", :string
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+
+Movie.create(title: "Titanic", year: 1997, director: "Ron Howard")
+Movie.create(title: "Palm Springs", year: 2020, director: "Adam")
+Movie.create(title: "Bridesmaids", year: 2015, director: "Jon")
